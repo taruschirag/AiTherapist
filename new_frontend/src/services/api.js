7fef90f81bbc0cbe4ec8c877e2275be00f3ac4bf
Chirag Jain <taruschirag@gmail.com>
2025-05-29 22:37:41 -0400

    fixed cors error

--- new_frontend/src/services/api.js
+++ new_frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { supabase } from './supabase';
 import { useState } from 'react';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://aitherapist-production.up.railway.app/';
 
 // Create axios instance
 const api = axios.create({
